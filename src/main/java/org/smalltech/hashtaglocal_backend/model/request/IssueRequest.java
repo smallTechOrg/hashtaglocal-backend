aaf88243098d7946ba9d2adae6a4d4f62871fa25
Vivek Agarwal <viveksinghalp@gmail.com>
2026-01-22 12:27:07 +0530

    swagger

--- src/main/java/org/smalltech/hashtaglocal_backend/model/request/IssueRequest.java
+++ src/main/java/org/smalltech/hashtaglocal_backend/model/request/IssueRequest.java
@@ -1,6 +1,7 @@
 package org.smalltech.hashtaglocal_backend.model.request;
 
 import java.util.ArrayList;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import java.util.List;
 import lombok.Data;
 
@@ -9,5 +10,6 @@ public class IssueRequest {
 	private String type;
 	private String description;
 	private LocationRequest location;
+	@JsonProperty("media_urls")
 	private List<MediaRequest> mediaUrls = new ArrayList<>();
 }
--- src/main/java/org/smalltech/hashtaglocal_backend/model/request/LocationRequest.java
+++ src/main/java/org/smalltech/hashtaglocal_backend/model/request/LocationRequest.java
@@ -1,12 +1,13 @@
 package org.smalltech.hashtaglocal_backend.model.request;
 
 import java.util.Map;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 @Data
 public class LocationRequest {
 	private String lat;
 	private String lng;
-
+	@JsonProperty("meta_data")
 	private Map<String, Object> metaData;
 }
