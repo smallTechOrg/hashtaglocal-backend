be9f74471627bfd6868179f72b38f49d0178d6a8
Sai Krishna Vennamaneni <saivnm5@gmail.com>
2026-01-26 17:13:03 +0530

    Limiting issues to six months

--- src/main/java/org/smalltech/hashtaglocal_backend/repository/IssueRepository.java
+++ src/main/java/org/smalltech/hashtaglocal_backend/repository/IssueRepository.java
@@ -1,11 +1,14 @@
 package org.smalltech.hashtaglocal_backend.repository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 import org.smalltech.hashtaglocal_backend.entity.IssueEntity;
 import org.smalltech.hashtaglocal_backend.model.IssueStatusModel;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -24,4 +27,9 @@ public interface IssueRepository extends JpaRepository<IssueEntity, Long> {
 
 	@EntityGraph(attributePaths = {"userEntity", "location", "location.locality"})
 	List<IssueEntity> findByStatusInOrderByCreatedAtDesc(List<IssueStatusModel> statuses);
+
+	@EntityGraph(attributePaths = {"userEntity", "location", "location.locality"})
+	@Query("SELECT i FROM IssueEntity i WHERE i.status IN :statuses AND i.createdAt >= :startDate ORDER BY i.createdAt DESC")
+	List<IssueEntity> findByStatusInAndCreatedAtAfterOrderByCreatedAtDesc(
+			@Param("statuses") List<IssueStatusModel> statuses, @Param("startDate") LocalDateTime startDate);
 }
