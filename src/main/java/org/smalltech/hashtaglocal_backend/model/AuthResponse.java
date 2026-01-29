84c3fc3b0572e2b8f7ac6ce85ce19d32596843b5
Vivek Agarwal <viveksinghalp@gmail.com>
2026-01-29 14:00:05 +0530

    model changes

--- src/main/java/org/smalltech/hashtaglocal_backend/model/AuthResponse.java
+++ src/main/java/org/smalltech/hashtaglocal_backend/model/AuthResponse.java
@@ -1,5 +1,11 @@
 package org.smalltech.hashtaglocal_backend.model;
 
+import lombok.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class AuthResponse {
 
 	private String accessToken;
@@ -9,51 +15,4 @@ public class AuthResponse {
 	private Long userId;
 	private Long providerId;
 	private String email;
-
-	public AuthResponse() {
-	}
-
-	public AuthResponse(String accessToken, long accessTokenExpiryTs, String refreshToken, long refreshTokenExpiryTs,
-			Long userId, Long providerId) {
-		this(accessToken, accessTokenExpiryTs, refreshToken, refreshTokenExpiryTs, userId, providerId, null);
-	}
-
-	public AuthResponse(String accessToken, long accessTokenExpiryTs, String refreshToken, long refreshTokenExpiryTs,
-			Long userId, Long providerId, String email) {
-		this.accessToken = accessToken;
-		this.accessTokenExpiryTs = accessTokenExpiryTs;
-		this.refreshToken = refreshToken;
-		this.refreshTokenExpiryTs = refreshTokenExpiryTs;
-		this.userId = userId;
-		this.providerId = providerId;
-		this.email = email;
-	}
-
-	public String getAccessToken() {
-		return accessToken;
-	}
-
-	public long getAccessTokenExpiryTs() {
-		return accessTokenExpiryTs;
-	}
-
-	public String getRefreshToken() {
-		return refreshToken;
-	}
-
-	public long getRefreshTokenExpiryTs() {
-		return refreshTokenExpiryTs;
-	}
-
-	public Long getUserId() {
-		return userId;
-	}
-
-	public Long getProviderId() {
-		return providerId;
-	}
-
-	public String getEmail() {
-		return email;
-	}
 }
--- src/main/java/org/smalltech/hashtaglocal_backend/model/GoogleUserResponse.java
+++ src/main/java/org/smalltech/hashtaglocal_backend/model/GoogleUserResponse.java
@@ -1,35 +1,14 @@
 package org.smalltech.hashtaglocal_backend.model;
 
-public class GoogleUserResponse {
+import lombok.*;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class GoogleUserResponse {
 	private String id;
 	private String email;
 	private String name;
 	private String picture;
-
-	public GoogleUserResponse() {
-	}
-
-	public GoogleUserResponse(String id, String email, String name, String picture) {
-		this.id = id;
-		this.email = email;
-		this.name = name;
-		this.picture = picture;
-	}
-
-	public String getId() {
-		return id;
-	}
-
-	public String getEmail() {
-		return email;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public String getPicture() {
-		return picture;
-	}
 }
