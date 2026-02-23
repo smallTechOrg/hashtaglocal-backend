efef631e46ff6161623e06463368a2cdba2f4b73
Vivek Agarwal <viveksinghalp@gmail.com>
2026-02-23 16:34:33 +0530

    validation error handling

---
+++ src/main/java/org/smalltech/hashtaglocal_backend/exception/ApiErrorResponse.java
@@ -0,0 +1,26 @@
+package org.smalltech.hashtaglocal_backend.exception;
+
+import java.util.List;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class ApiErrorResponse {
+
+	private ErrorEnvelope error;
+
+	@Data
+	@Builder
+	public static class ErrorEnvelope {
+		private String message;
+		private List<ApiError> errors;
+	}
+
+	@Data
+	@Builder
+	public static class ApiError {
+		private String type; // VALIDATION, AUTH, PERMISSION, GEO, etc.
+		private String message; // human readable
+	}
+}
