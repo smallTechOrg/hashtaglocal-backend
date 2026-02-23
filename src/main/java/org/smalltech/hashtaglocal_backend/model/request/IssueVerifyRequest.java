eab89bf6c9e44d681f16fa7954d05f979f398aa5
Vivek Agarwal <viveksinghalp@gmail.com>
2026-02-23 19:40:38 +0530

    fix

--- src/main/java/org/smalltech/hashtaglocal_backend/model/request/IssueVerifyRequest.java
+++ src/main/java/org/smalltech/hashtaglocal_backend/model/request/IssueVerifyRequest.java
@@ -2,7 +2,6 @@ package org.smalltech.hashtaglocal_backend.model.request;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.Valid;
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import java.util.List;
 import lombok.Data;
@@ -12,7 +11,7 @@ public class IssueVerifyRequest {
 
 	@Valid
 	@JsonProperty("issue_action")
-	@NotBlank(message = "issue_action is required")
+	@NotNull(message = "issue_action is required")
 	private IssueActionRequest issueAction;
 
 	@Data
